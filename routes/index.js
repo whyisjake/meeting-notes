13a1c79f87f899061228f23d1215a4db36a3d8d6
Jake Spurlock <whyisjake@gmail.com>
2020-05-27 22:45:12 -0700

    Since Mooch is such a great guy, let's add quarter params.

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,40 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const WPCOM = require('wpcom');
+const WPCOM = require("wpcom");
 const wpcom = WPCOM();
-const moment = require('moment');
-const _ = require('lodash');
+const moment = require("moment");
+const _ = require("lodash");
 
-router.get('/', function(req, res, next) {
-  const weeks = _.get(req, 'query.weeks', 2);
-  const twoWeeksAgo = moment().subtract(weeks, 'weeks').format();
+router.get("/", function (req, res, next) {
+  const weeks = _.get(req, "query.weeks", 2);
+  const quarter = _.get(req, "query.quarter", false);
+  const year = _.get(req, "query.year", false);
+
+  let start = moment().subtract(weeks, "weeks").format();
+  let end;
+
+  if (quarter && year) {
+    let time = moment(year).add(quarter - 1, "quarter");
+    start = time.startOf("quarter").format();
+    end = time.endOf("quarter").format();
+  }
+
   wpcom
-  .site( 'lobby.vip.wordpress.com' )
-	.postsList( 
-    {
-      after: twoWeeksAgo
+    .site("lobby.vip.wordpress.com")
+    .postsList({
+      after: start,
+      before: end,
+      number: 100,
     })
-		.then( list => {
-      res.render('index', { pageTitle: 'Recent Lobby Posts', posts: list.posts });
-    } )
-		.catch( 
-      error => {
-        console.error(error)
-      } 
-    );
+    .then((list) => {
+      res.render("index", {
+        pageTitle: "Recent Lobby Posts",
+        posts: list.posts,
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 });
 
 module.exports = router;
